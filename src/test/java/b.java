4ba7d84ad43dfa6daa4c276ff2c3f64e3b47d878
rava92 <abilkadirov.ra@gmail.com>
2019-10-13 18:21:21 -0400

    hhhh

--- src/test/java/b.java
+++ src/test/java/b.java
@@ -1,5 +1,11 @@
 public class b {
     public static void main(String[] args) {
         System.out.println("1 one");
+        System.out.println("second ");
+        System.out.println("second ");
+        System.out.println("second ");
+        System.out.println("second ");
+        System.out.println("second ");
+        System.out.println("second ");
     }
 }
--- src/test/java/bb.java
+++ src/test/java/bb.java
@@ -1,5 +1,12 @@
 public class bb {
     public static void main(String[] args) {
         int main =9;
+        int main2 =9;
+        int main33 =9;
+        int main34 =9;
+        int main23 =9;
+        int main45 =9;
+        int main234=9;
+
     }
 }
